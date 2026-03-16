41a93864d20152fda2532c0b3273aedc6f848caa
gunelismayilzada <gunelismayilzad7@gmail.com>
2026-03-16 19:37:07 +0400

    Short description of changes

--- app/routers/main.py
+++ app/routers/main.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
 
+from app.services import event_service
+
 main_bp = Blueprint('main', __name__)
 
 
 @main_bp.get('/')
 def index():
-    return render_template('main/index.html')
+    events = event_service.list_events({})
+    return render_template('main/index.html', events=events)
